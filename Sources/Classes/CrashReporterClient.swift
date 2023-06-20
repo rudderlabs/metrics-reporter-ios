e115a01ca8d2db7e958cbfa68668882f9bb8f91f
Pallab Maiti <mynamepallabmaiti@gmail.com>
2023-06-20 21:53:51 +0530

    modified necessary files

--- Sources/Classes/CrashReporterClient.swift
+++ Sources/Classes/CrashReporterClient.swift
@@ -11,7 +11,7 @@ import RSCrashReporter
 public class CrashReporterClient {
     
     public init() {
-        RSCrashReporter.start(withApiKey: "df5da4234cd9883c66557a2b9b75c082")
+//        RSCrashReporter.start(withApiKey: "df5da4234cd9883c66557a2b9b75c082")
     }
     
     
