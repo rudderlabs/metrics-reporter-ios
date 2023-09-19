2004c6782a244f2906943b3fe4b9dbfdd433f87c
Pallab Maiti <mynamepallabmaiti@gmail.com>
2023-09-19 15:30:23 +0530

    chore: updated Package.swift

--- Package.swift
+++ Package.swift
@@ -15,19 +15,21 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "RudderKit", url: "https://github.com/rudderlabs/rudder-ios-kit", from: "1.4.0"),
+        .package(name: "RSCrashReporter", url: "https://github.com/rudderlabs/crash-reporter-ios", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "MetricsReporter",
             dependencies: [
                 .product(name: "RudderKit", package: "RudderKit"),
+                .product(name: "RSCrashReporter", package: "RSCrashReporter"),
             ],
             path: "Sources",
             sources: ["Classes/"]
         ),
         .testTarget(
             name: "MetricsReporterTests",
-            dependencies: ["MetricsReporter", "RudderKit"],
+            dependencies: ["MetricsReporter", "RudderKit", "RSCrashReporter"],
             path: "MetricsReporterTests"
         ),
     ]
